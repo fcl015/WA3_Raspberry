407b3f4569e2ac1c186a10b65ad5ab5433b7d49d
fcl015 <camilo.lozoya@itesm.mx>
2018-06-15 10:32:44 -0600

    Substitute RPi video server for mjpeg server

--- scripts/start_RPi_video.sh
+++ scripts/start_RPi_video.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          RPi video server
+# Required-Start:    $local_fs $remote_fs $network $syslog $named
+# Required-Stop:     $local_fs $remote_fs $network $syslog $named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: RPi video server
+# Description:       Start the video server and associated helpers
+#  This script will start RPi video, and possibly all associated instances.
+### END INIT INFO
 cd /home/pi/Code/WA3/WA3_Raspberry/log
 date >> /home/pi/Code/WA3/WA3_Raspberry/log/log_video.txt
 echo "Starting RPi video services" >> /home/pi/Code/WA3/WA3_Raspberry/log/log_video.txt
--- scripts/start_mjpg_streamer.sh
+++ scripts/start_mjpg_streamer.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          mjpg streamer
+# Required-Start:    $local_fs $remote_fs $network $syslog $named
+# Required-Stop:     $local_fs $remote_fs $network $syslog $named
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: mjpg video server
+# Description:       Start the video server and associated helpers
+#  This script will start mjpg streamer, and possibly all associated instances.
+### END INIT INFO
 cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
 date >> /home/pi/Code/WA3/WA3_Raspberry/log/log_video.txt
 echo "Starting mjpg-streamer video services" >> /home/pi/Code/WA3/WA3_Raspberry/log/log_video.txt
